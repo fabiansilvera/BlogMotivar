4abbe595219a556212301f7c7d3336c078e920c0
Fabian Silvera <fabiansilvera@gmail.com>
2022-07-18 07:57:54 -0300

    correccion en js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    iniciarApp()
-})
-function iniciarApp() {
-    generarSeccion()
-    usarDatos()
-}
-let datos = {
+let IdCounter = 0;
+const datos = {
     titulo: '',
     metodo: '',
     mensaje: '',
-}
+};
 const titulo = document.querySelector('#titulo');
 const metodo = document.querySelector('#metodo');
 const mensaje = document.querySelector('#mensaje');
 const input = document.querySelector('.input');
-const listaTarea = document.querySelector('#lista-tareas')
+const errorMensaje = document.querySelector('#error');
 const formulario = document.querySelector('.formulario');
-const template = document.getElementById('template').content
-const fragment = document.createDocumentFragment();
+const crearSeccion = document.querySelector('#crearSeccion');
 
 //let seccion =
 
 titulo.addEventListener('input', leertexto);
 metodo.addEventListener('input', leertexto);
 mensaje.addEventListener('input', leertexto);
+
+crearSeccion.addEventListener('click',e =>{
+    botonCerrar(e)
+})
 
 formulario.addEventListener('submit',e => {
     e.preventDefault()
@@ -32,8 +29,7 @@ formulario.addEventListener('submit',e => {
         mostrarError('Todos los campos son obligatorios');
         return;
         } else {
-            generarSeccion(e) 
-            listaTarea.appendChild(cerrarSeccion)
+            generarSeccion()
         };
 });
 function leertexto(e) {
@@ -45,41 +41,34 @@ function mostrarError(mensaje) {
     error.textContent = mensaje;
     error.classList.add('error');
 
-    formulario.appendChild (error);
+    errorMensaje.appendChild (error);
     setTimeout(()=>{
         error.remove();
     }, 2000);
 };
 
-const generarSeccion = (e) => {
-    const dato = {
-        id: Date.now(),
-        titulo: datos.titulo,
-        metodo: datos.metodo,
-        mensaje: datos.mensaje,
-        estado: false
-    }
-    datos[dato.id] = dato
-    formulario.reset()
-    usarDatos()
-    
+const generarSeccion = () => {
+    IdCounter++;
+    crearSeccion.innerHTML += `
+    <div class="seccionPersonal" id="${IdCounter}">
+    <h2 class="centrar-texto">"${datos.titulo}"</h2>
+    <h3 class="seccion-conectar__metodo">"${datos.metodo}"</h3>
+    <p class="seccion-conectar__parrafo">"${datos.mensaje}"</p>
+     <div class="posicion-boton">
+      <svg xmlns="http://www.w3.org/2000/svg" class="botonCerrar" id="${IdCounter}" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+       <line x1="4" y1="7" x2="20" y2="7" />
+       <line x1="10" y1="11" x2="10" y2="17" />
+       <line x1="14" y1="11" x2="14" y2="17" />
+       <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+       <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+      </svg>
+    </div>
+    `
 }
-const usarDatos = () => {
-    listaTarea.innerHTML = ''
-    Object.values(datos).forEach(dato => {
-    const clone = template.cloneNode(true)
-    clone.querySelector('h2').textContent = dato.titulo
-    clone.querySelector('h3').textContent = dato.metodo
-    clone.querySelector('p').textContent = dato.mensaje
-    fragment.appendChild(clone)
-    })
-    listaTarea.appendChild(fragment) 
-    
-    const cerrarSeccion= document.createElement('p');
-    cerrarSeccion.textContent = 'X';
-    cerrarSeccion.classList.add('btn-cerrar');
-    cerrarSeccion.classList.add('btn-cerrar__posicion');
-    cerrarSeccion.onclick = function() {
-       listaTarea.remove();
-    }
- }
+
+const botonCerrar = e=> {
+    if (e.target.classList.contains("botonCerrar")) {
+        document.getElementById([e.target.id]).remove();
+}
+}
